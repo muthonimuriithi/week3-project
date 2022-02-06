8736abb4c753290b91dc6df884c6c88bc3c4c944
muthonimuriithi <loissmuthoni@student.moringaschool.com>
2022-02-06 21:49:37 +0300

    added the if function

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function check(){
     var a=0;
+    
     var q1=document.quiz.question1.value;
     var q2=document.quiz.question2.value;
     var q3=document.quiz.question3.value;
     var q4=document.quiz.question4.value;
     var q5=document.quiz.question5.value;
 
-
-
+    if(question1=="HTML") {a++}
+    if(question2=="JS") {a++}
+    if(question3=="Yes") {a++}
+    if(question4=="script") {a++}
+    if(question5=="Both the head and body section") {a++}
+    document.write(a);
 }
 
 
